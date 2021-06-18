d5fab1d8571309f512e819fb2b476c56f71a7ed6
Juliomar de Matos Souza <infoexpress2014@gmail.com>
2021-06-17 21:04:14 -0300

    Configurando docker e postgres

---
+++ pypro/aperitivos/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.urls import reverse
+
+
+class Video:
+    def __init__(self, slug, titulo, youtube_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.youtube_id = youtube_id
+    def get_absolute_url(self):
+        return reverse('aperitivo:video', args=(self.slug,))
+
+
+videos = [
+    Video('motivacao', 'Video Aperitivo: Motivação', 'GEA-QiQeztU'),
+    Video('instalacao-windows', 'Instalacão Windows',125645)
+    ]
+
+videos_dct = {v.slug: v for v in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
+
+def video(request, slug):
+    video = videos_dct[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
